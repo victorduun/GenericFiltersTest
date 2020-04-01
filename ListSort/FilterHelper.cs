22c4471b613f34cd206b001d3db1cd72a908bc9c
victorduun <43340964+victorduun@users.noreply.github.com>
2020-04-01 20:42:12 +0200

    Add project files.

---
+++ ListSort/FilterHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Dynamic.Core;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ListSort
+{
+    public enum SortOrder { Ascending, Descending };
+
+    public enum FilterType { Contains, DoesNotContain, StartsWith, DoesNotStartWith, Is, IsNot, IsBlank, IsNotBlank };
+    public static class EnumExtensionMethods
+    {
+        public static string GetString(this SortOrder order)
+        {
+            return order switch
+            {
+                SortOrder.Ascending => "asc",
+                SortOrder.Descending => "desc",
+                _ => "error",
+            };
+        }
+        public static string GetString(this FilterType type)
+        {
+            return type switch
+            {
+
+                _ => "test",
+            };
+        }
+
+    }
+
+
+    public class FilterHelper<T>
+    {
+
+
+
+        public static List<T> DynamicSort(List<T> listToSort, SortOrder sortOrder, string attributeToSortBy)
+        {
+            return listToSort.AsQueryable().OrderBy(attributeToSortBy + " " + sortOrder.GetString()).ToList();
+
+        }
+
+        public static List<T> GenericFilter(List<T> listToFilter, string propertyToFilter, string filterValue, FilterType filter)
+        {
+           
+            string query = "";
+            switch (filter)
+            {
+                case FilterType.Contains:
+                    query = String.Format("t=>t.{0}.Contains(\"{1}\")", propertyToFilter, filterValue);
+                    break;
+                case FilterType.DoesNotContain:
+                    query = String.Format("t=>!t.{0}.Contains(\"{1}\")", propertyToFilter, filterValue);
+                    break;
+                case FilterType.DoesNotStartWith:
+                    query = String.Format("t=>!t.{0}.StartsWith(\"{1}\")", propertyToFilter, filterValue);
+                    break;
+                case FilterType.Is:
+                    query = String.Format("t=>t.{0} == \"{1}\"", propertyToFilter, filterValue);
+                    break;
+                case FilterType.IsBlank:
+                    query = String.Format("t=>String.IsNullOrEmpty(t.{0})", propertyToFilter);
+                    break;
+                case FilterType.IsNot:
+                    query = String.Format("t=>t.{0} != \"{1}\"", propertyToFilter, filterValue);
+                    break;
+                case FilterType.IsNotBlank:
+                    query = String.Format("t=>!String.IsNullOrEmpty(t.{0})", propertyToFilter);
+                    break;
+                case FilterType.StartsWith:
+                    query = String.Format("t=>t.{0}.StartsWith(\"{1}\")", propertyToFilter, filterValue);
+                    break;
+                default:
+                    query = String.Format("t=>!String.IsNullOrEmpty(t.{0})", propertyToFilter); // Default to is not blank
+                    break;
+            }
+            return listToFilter.AsQueryable().Where(query).ToList();
+        }
+
+
+    }
+}
---
+++ ListSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListSort
+{
+
+    public class Things
+    {
+        public Things(string name, int num)
+        {
+            tname = name;
+            number = num;
+        }
+        public string tname { get; set; }
+        public int number { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Things> thingList = new List<Things>();
+            thingList.Add(new Things("betty", 1));
+            thingList.Add(new Things("catherine", 2));
+            thingList.Add(new Things("abel", 3));
+
+            
+            var sortedList = FilterHelper<Things>.DynamicSort(thingList, SortOrder.Descending, "tname");
+
+            var filteredList1 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "bet", FilterType.Contains);
+            var filteredList2 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "bet", FilterType.DoesNotContain);
+            var filteredList3 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "ab", FilterType.DoesNotStartWith);
+            var filteredList4 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "betty", FilterType.Is);
+            var filteredList5 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "bet", FilterType.IsBlank);
+            var filteredList6 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "bet", FilterType.IsNot);
+            var filteredList7 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "", FilterType.IsNotBlank);
+            var filteredList8 = FilterHelper<Things>.GenericFilter(sortedList, "tname", "cat", FilterType.StartsWith);
+
+
+        }
+    }
+
+       
+       
+
+
+}
+
+

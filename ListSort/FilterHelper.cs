a42e8dab78d6ae5063886ce4494a6093b538e41c
victorduun <43340964+victorduun@users.noreply.github.com>
2020-04-02 23:05:23 +0200

    Filter by version

--- ListSort/FilterHelper.cs
+++ ListSort/FilterHelper.cs
@@ -10,9 +10,11 @@ namespace ListSort
 {
     public enum SortOrder { Ascending, Descending };
 
-    public enum StringFilterType { Contains, DoesNotContain, StartsWith, DoesNotStartWith, Is, IsNot, IsBlank, IsNotBlank };
+    public enum StringFilter { Contains, DoesNotContain, StartsWith, DoesNotStartWith, Is, IsNot, IsBlank, IsNotBlank };
     public enum DateTimeFilterType { Is, IsNot, IsAfter, IsOnOrAfter, IsBefore, IsOnOrBefore, IsBlank, IsNotBlank };
-    public enum DoubleFilterType { Is, IsNot, IsAfter, IsOnOrAfter, IsBefore, IsOnOrBefore, IsBlank, IsNotBlank };
+    public enum NumberFilterType { IsLessThan, IsLessThanOrEqualTo, IsGreaterThan, IsGreaterThanOrEqualTo, Is, IsNot, IsBlank, IsNotBlank };
+
+    public enum VersionFilter { Is, IsNot, IsEarlier, IsLater, IsSameMajorVersion, IsEarlierMajorVersion, IsLaterMajorVersion, IsSameOrLater, IsSameOrEarlier, IsBlank, IsNotBlank };
     public static class EnumExtensionMethods
     {
         public static string GetString(this SortOrder order)
@@ -24,7 +26,7 @@ namespace ListSort
                 _ => "error",
             };
         }
-        public static string GetString(this StringFilterType type)
+        public static string GetString(this StringFilter type)
         {
             return type switch
             {
@@ -35,6 +37,65 @@ namespace ListSort
 
     }
 
+    public static class ListFilterExtensions
+    {
+        public static IEnumerable<TModel> AdvancedFilter<TModel, TProperty>(
+            this IEnumerable<TModel> enumerable, 
+            Expression<Func<TModel, TProperty>> propertyToFilter, 
+            StringFilter filter, 
+            string filterValue)
+        {
+            MemberExpression memberExpression = (MemberExpression)propertyToFilter.Body;
+            string memberName = memberExpression.Member.Name;
+
+
+            string query = filter switch
+            {
+                StringFilter.Contains => String.Format("t=>t.{0}.Contains(\"{1}\")", memberName, filterValue),
+                StringFilter.DoesNotContain => String.Format("t=>!t.{0}.Contains(\"{1}\")", memberName, filterValue),
+                StringFilter.DoesNotStartWith => String.Format("t=>!t.{0}.StartsWith(\"{1}\")", memberName, filterValue),
+                StringFilter.Is => String.Format("t=>t.{0} == \"{1}\"", memberName, filterValue),
+                StringFilter.IsBlank => String.Format("t=>String.IsNullOrEmpty(t.{0})", memberName),
+                StringFilter.IsNot => String.Format("t=>t.{0} != \"{1}\"", memberName, filterValue),
+                StringFilter.IsNotBlank => String.Format("t=>!String.IsNullOrEmpty(t.{0})", memberName),
+                StringFilter.StartsWith => String.Format("t=>t.{0}.StartsWith(\"{1}\")", memberName, filterValue),
+                _ => String.Format("t=>!String.IsNullOrEmpty(t.{0})", memberName), // Default to is not blank
+            };
+            return enumerable.AsQueryable().Where(query).ToList();
+        }
+
+        public static IEnumerable<TModel> AdvancedFilter<TModel, TProperty>(
+            this IEnumerable<TModel> enumerable,
+            Expression<Func<TModel, TProperty>> propertyToFilter,
+            VersionFilter filter,
+            Version filterValue)
+        {
+            MemberExpression memberExpression = (MemberExpression)propertyToFilter.Body;
+            string memberName = memberExpression.Member.Name;
+
+            //Sort through null/not null first to prevent later errors
+            IEnumerable<TModel> nullVersions = enumerable.AsQueryable().Where($"t=>t.{memberName} == null");
+            IEnumerable<TModel> validVersions = enumerable.AsQueryable().Where($"t=>t.{memberName} != null");
+
+            return filter switch
+            {
+                VersionFilter.Is => validVersions.AsQueryable().Where($"t=>t.{memberName} = @0", filterValue),
+                VersionFilter.IsBlank => nullVersions,
+                VersionFilter.IsEarlier => validVersions.AsQueryable().Where($"t=>t.{memberName} < @0", filterValue),
+                VersionFilter.IsEarlierMajorVersion => validVersions.AsQueryable().Where($"t=>t.{memberName}.Major < @0", filterValue.Major),
+                VersionFilter.IsLater => validVersions.AsQueryable().Where($"t=>t.{memberName}.Major > @0", filterValue.Major),
+                VersionFilter.IsLaterMajorVersion => validVersions.AsQueryable().Where($"t=>t.{memberName}.Major > @0", filterValue.Major),
+                VersionFilter.IsSameMajorVersion => validVersions.AsQueryable().Where($"t=>t.{memberName}.Major = @0", filterValue.Major),
+                VersionFilter.IsNot => validVersions.AsQueryable().Where($"t=>t.{memberName} != @0", filterValue),
+                VersionFilter.IsNotBlank => validVersions,
+                VersionFilter.IsSameOrEarlier => validVersions.AsQueryable().Where($"t=>t.{memberName} <= @0", filterValue),
+                VersionFilter.IsSameOrLater => validVersions.AsQueryable().Where($"t=>t.{memberName} >= @0", filterValue),
+                _ => validVersions, // Default to is not blank
+            };
+
+
+        }
+    }
 
     public class FilterHelper<T>
     {
@@ -47,34 +108,35 @@ namespace ListSort
 
         }
 
-        public static List<T> GenericStringFilter(List<T> listToFilter, string propertyToFilter, StringFilterType filter, string filterValue)
+
+        public static List<T> GenericStringFilter(List<T> listToFilter, string propertyToFilter, StringFilter filter, string filterValue)
         {
            
             string query = "";
             switch (filter)
             {
-                case StringFilterType.Contains:
+                case StringFilter.Contains:
                     query = String.Format("t=>t.{0}.Contains(\"{1}\")", propertyToFilter, filterValue);
                     break;
-                case StringFilterType.DoesNotContain:
+                case StringFilter.DoesNotContain:
                     query = String.Format("t=>!t.{0}.Contains(\"{1}\")", propertyToFilter, filterValue);
                     break;
-                case StringFilterType.DoesNotStartWith:
+                case StringFilter.DoesNotStartWith:
                     query = String.Format("t=>!t.{0}.StartsWith(\"{1}\")", propertyToFilter, filterValue);
                     break;
-                case StringFilterType.Is:
+                case StringFilter.Is:
                     query = String.Format("t=>t.{0} == \"{1}\"", propertyToFilter, filterValue);
                     break;
-                case StringFilterType.IsBlank:
+                case StringFilter.IsBlank:
                     query = String.Format("t=>String.IsNullOrEmpty(t.{0})", propertyToFilter);
                     break;
-                case StringFilterType.IsNot:
+                case StringFilter.IsNot:
                     query = String.Format("t=>t.{0} != \"{1}\"", propertyToFilter, filterValue);
                     break;
-                case StringFilterType.IsNotBlank:
+                case StringFilter.IsNotBlank:
                     query = String.Format("t=>!String.IsNullOrEmpty(t.{0})", propertyToFilter);
                     break;
-                case StringFilterType.StartsWith:
+                case StringFilter.StartsWith:
                     query = String.Format("t=>t.{0}.StartsWith(\"{1}\")", propertyToFilter, filterValue);
                     break;
                 default:
--- ListSort/Program.cs
+++ ListSort/Program.cs
@@ -7,16 +7,18 @@ namespace ListSort
 
     public class Thing
     {
-        public Thing(string name, string label, int num)
+        public Thing(string name, string label, int num, Version vers)
         {
             tname = name;
             tlabel = label;
             number = num;
+            version = vers;
         }
         
         public string tname { get; set; }
         public string tlabel { get; set; }
         public int number { get; set; }
+        public Version version { get; set; }
     }
     class Program
     {
@@ -32,15 +34,17 @@ namespace ListSort
             Console.WriteLine("Hello World!");
             List<Thing> thingList = new List<Thing>
             {
-                new Thing("betty", "label1", 1),
-                new Thing("catherine", "label3", 2),
-                new Thing("abel", "label4", 3),
-                new Thing("dimitri", "label7", 4),
-                new Thing("eigil", "label99", 5),
-                new Thing("fritz", "label77", 6),
-                new Thing("gertrude", "label45", 7),
-                new Thing("", "label45", 8)
+                new Thing("betty", "label1", 1, new Version("15.32.3")),
+                new Thing("catherine", "label3", 2, new Version("2.2.3")),
+                new Thing("abel", "label4", 3, new Version("1.23.3")),
+                new Thing("dimitri", "label7", 4, new Version("1.22.3")),
+                new Thing("eigil", "label99", 5, new Version("2.24.3")),
+                new Thing("fritz", "label77", 6, new Version("1.27.3")),
+                new Thing("gertrude", "label45", 7, new Version("1.2.4")),
+                new Thing("", "label45", 8, null)
             };
+
+
 
             var sortedList1 = FilterHelper<Thing>.DynamicSort(thingList, SortOrder.Ascending, GetPropertyName((Thing t) => t.tlabel));
             var sortedList2 = FilterHelper<Thing>.DynamicSort(thingList, SortOrder.Ascending, GetPropertyName((Thing t) => t.tname));
@@ -51,15 +55,31 @@ namespace ListSort
             var sortedList6 = FilterHelper<Thing>.DynamicSort(thingList, SortOrder.Descending, GetPropertyName((Thing t) => t.number));
 
 
-            var filteredList1 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.Contains, "bet" );
-            var filteredList2 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.DoesNotContain, "bet");
-            var filteredList3 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.DoesNotStartWith, "ab");
-            var filteredList4 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.Is, "betty");
-            var filteredList5 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.IsBlank, string.Empty);
-            var filteredList6 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.IsNot, "fritz");
-            var filteredList7 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.IsNotBlank, string.Empty);
-            var filteredList8 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilterType.StartsWith, "cat");
+            var filteredList1 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.Contains, "bet" );
+            var filteredList2 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.DoesNotContain, "bet");
+            var filteredList3 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.DoesNotStartWith, "ab");
+            var filteredList4 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.Is, "betty");
+            var filteredList5 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.IsBlank, string.Empty);
+            var filteredList6 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.IsNot, "fritz");
+            var filteredList7 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.IsNotBlank, string.Empty);
+            var filteredList8 = FilterHelper<Thing>.GenericStringFilter(thingList, GetPropertyName((Thing t) => t.tname), StringFilter.StartsWith, "cat");
 
+
+            //List extension method doing the same thing but simpler syntax
+            var filteredList121 = thingList.AdvancedFilter((t => t.tname), StringFilter.Contains, "bet");
+
+            //With version filtering support
+            var filteredList6666 = thingList.AdvancedFilter((t => t.version), VersionFilter.Is, thingList[0].version);
+            var filteredList26676 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsBlank, null);
+            var filteredList66786 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsEarlier, thingList[1].version);
+            var filteredList66676 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsEarlierMajorVersion, thingList[0].version);
+            var filteredList666766 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsLater, thingList[0].version);
+            var filteredList66166 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsLaterMajorVersion, thingList[0].version);
+            var filteredList645666 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsSameMajorVersion, new Version("1.2"));
+            var filteredList664566 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsNot, new Version("15.32.3"));
+            var filteredList64666 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsNotBlank, null);
+            var filteredList66766 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsSameOrEarlier, new Version("2.24.3"));
+            var filteredList66966 = thingList.AdvancedFilter((t => t.version), VersionFilter.IsSameOrLater, new Version("2.24.3"));
 
         }
     }
